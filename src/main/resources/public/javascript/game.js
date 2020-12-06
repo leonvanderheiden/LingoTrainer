bb43f5b80a84f0f656d9808bb8734fd6dccf09e3
leonvanderheiden <43435696+leonvanderheiden@users.noreply.github.com>
2020-12-06 17:41:14 +0100

    Maken van een game in de front-end is mogelijk

--- src/main/resources/public/javascript/game.js
+++ src/main/resources/public/javascript/game.js
@@ -1,39 +1,58 @@
-function createScore(score) {
-    var scoreObject = new Object();
-    scoreObject.score = score;
+const fetchScore = async args => {
+    const res = await fetch(`/score`, {
+        method: "POST",
+        body: JSON.stringify({ score: 0 }),
+        headers: {
+            "Accept": "application/json", "Content-Type": "application/json"
+        }
+    });
+    const body = await res.json();
+    return body;
+};
+const fetchRound = async args => {
+    const res = await fetch(`/round`, {
+        method: "POST",
+        body: JSON.stringify({ roundType: "5 letterwoord" }),
+        headers: {
+            "Accept": "application/json", "Content-Type": "application/json"
+        }
+    });
+    const body = await res.json();
+    return body;
+};
 
-    var scoreJsonString = JSON.stringify(scoreObject);
+function createRound(type) {
+    var round = new Object();
+    round.roundType = type;
 
-    fetch("/score", {
+    var roundJsonString = JSON.stringify(round);
+
+    fetch("/round", {
         method: 'POST',
-        headers: { 'Accept': 'application/json', 'Content-Type': 'application/json',}, body: scoreJsonString })
+        headers: { 'Accept': 'application/json', 'Content-Type': 'application/json',}, body: roundJsonString })
         .then(response => response.json())
-        .then(function(scoreInfo) {
-            console.log(scoreInfo);
-            return JSON.stringify(scoreInfo);
+        .then(function(roundInfo) {
+            console.log(roundInfo);
+            return JSON.stringify(roundInfo);
         })
 }
 
-function newGame() {
+async function newGame() {
     var game = new Object();
-    game.score = createScore(0);
+    game.score = await fetchScore();
+
+    game.rounds = new Array();
+    game.rounds.push(await fetchRound());
 
     var gameJsonString = JSON.stringify(game);
-    console.log(gameJsonString);
-    alert(gameJsonString);
+    var user = JSON.parse(sessionStorage.getItem("loggedUser"));
 
+    fetch("/game/" + user.id, {
+        method: 'POST',
+        headers: { 'Accept': 'application/json', 'Content-Type': 'application/json',}, body: gameJsonString })
+        .then(response => response.json())
+        .then(function(gameInfo) {
+            console.log(gameInfo);
+        })
 
-
-    /*var username = document.forms["login"]["username"].value
-    alert(username);
-
-    fetch("/user/" + username, {method: "GET"})
-        .then(function (response) {
-            return response.json();
-
-        }).then(function (users) {
-
-            console.log(users);
-            document.querySelector(".example").textContent = users.name;
-        })*/
 }
